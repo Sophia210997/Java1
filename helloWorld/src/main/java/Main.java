b8db0c4e6aa26e89bb12c235e1b80f6e60549d15
sophia.secretary <sophsecretary@gmail.com>
2021-10-18 12:35:41 +0100

    Intro to data types

--- helloWorld/src/main/java/Main.java
+++ helloWorld/src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "Sophia";
+        int age = 20;
+        long age2 = 20L;
+        double pi = 3.14;
+        float pi2 = 3.14f;
+        char letter = 'A';
     }
 }
+//variable is a placeholder to store an element or an object
+//integer only allows you to store whole numbers
+//Double can be used for decimal numbers
+//Char and single quotes for a character
+//Float allows you to store really long decimals, and you need to end it with the lowercase l.
